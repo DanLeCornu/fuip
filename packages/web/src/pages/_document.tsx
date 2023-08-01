6482ed07a0fd4e52601465180baa36b4278e9f48
Daniel Le Cornu <dan@noquarter.co>
2023-08-01 12:45:46 +0100

    add hotjar script

--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -26,6 +26,7 @@ export default class AppDocument extends Document {
           />
           {IS_PRODUCTION && (
             <div className="container">
+              {/* GOOGLE ANALYTICS */}
               <Script src="https://www.googletagmanager.com/gtag/js?id=G-3XQEJLV0LG" />
               <Script id="ga-tag">
                 {`
@@ -33,6 +34,19 @@ export default class AppDocument extends Document {
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
                   gtag('config', 'G-3XQEJLV0LG');
+                `}
+              </Script>
+              {/* HOTJAR */}
+              <Script id="hotjar">
+                {`
+                  (function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:3596037,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
                 `}
               </Script>
             </div>
